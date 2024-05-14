06b7aefbb8942de7ee56fe6e9d8f14856dec7902
matthewzenm <matthewzenm@163.com>
2024-05-14 19:13:22 +0800

    Change compile platform.

--- problem2/program2-4.c
+++ problem2/program2-4.c
@@ -1,25 +1,26 @@
 // Newton method with Aikten acceleration
-// unfinished
 #include <stdio.h>
 #include <math.h>
 
 double f1(double x);
 double df1(double x);
+double itf1(double x);
 double f2(double x);
 double df2(double x);
+double itf2(double x);
 
 int main()
 {
     double x=-1;
     while (fabs(f1(x)/df1(x))>0.5e-8)
     {
-        x=x-f1(x)/df1(x);
+        x=(x*itf1(itf1(x))-itf1(x)*itf1(x))/(x-2*itf1(x)+itf1(itf1(x)));
     }
     printf("%.8lf\n",x);
     x=2;
     while (fabs(f2(x)/df2(x))>0.5e-8)
     {
-        x=x-f2(x)/df2(x);
+        x=(x*itf2(itf2(x))-itf2(x)*itf2(x))/(x-2*itf2(x)+itf2(itf2(x)));
     }
     printf("%.8lf",x);
     return 0;
@@ -35,6 +36,11 @@ double df1(double x)
     return (cos(x)-6.0);
 }
 
+double itf1(double x)
+{
+    return (x-f1(x)/df1(x));
+}
+
 double f2(double x)
 {
     return (log(x)+x*x-3.0);
@@ -43,4 +49,9 @@ double f2(double x)
 double df2(double x)
 {
     return (1/x+2*x);
+}
+
+double itf2(double x)
+{
+    return (x-f2(x)/df2(x));
 }
